925417bd6effb03866cfcd6134be596703b9518e
Varshini <varshini.reddy4@gmail.com>
2020-07-15 16:56:19 -0500

    Sorted Square.

---
+++ Arrays/SortedSquare.py
@@ -0,0 +1,11 @@
+"""
+Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number,
+also in sorted non-decreasing order.
+Input: [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+"""
+
+class Square:
+    def sortedSquares(self,A):
+        
+
