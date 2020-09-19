5c6419da80ca1f8333219f940c20b431c1ec95c8
Varshini <varshini.reddy4@gmail.com>
2020-09-19 12:18:13 -0500

    bst sequence

---
+++ CCI/Trees and Graphs/BSTSequence.py
@@ -0,0 +1,7 @@
+"""
+BST Sequences: A binary search tree was created by traversing through an array from left to right and inserting each
+element. Given a binary search tree with distinct elements, print all possible arrays that could have led to this tree.
+#39, #48, #66, #82
+
+"""
+
