d36d2786705fe285a5c1784c710c16d55083a731
Varshini <varshini.reddy4@gmail.com>
2020-10-05 12:27:00 -0500

    ThreeSum

--- Arrays/ThreeSum.py
+++ Arrays/ThreeSum.py
@@ -1,37 +1,33 @@
+
+
+
+
+#Two pointers
 class Solution:
     def threeSum(self, nums):
-        out = []
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                for k in range(len(nums)-1, j, -1):
-                    if (nums[i] + nums[j] + nums[k] == 0):
-                        res = [nums[i],nums[j],nums[k]]
-                        res.sort()
-                        out.append(res)
-        # # result = []
-        # import copy
-        # result = copy.deepcopy(out)
-        # if len(out) == 1 :
-        #     return out
-        # else:
-        #     for m in range(len(out)):
-        #         for n in range(m + 1, len(out)):
-        #             if out[m] == out[n]:
-        #                 print(n)
-        #                 del result[m]
-        #
-        #                 if len(out) == 1 :
-        #                     return result
-
         result = []
-
-        for i in range(len(out)):
-            if (result.__contains__(out[i])):
-                print()
-            else :
-                result.append(out[i])
-
+        nums.sort()
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            beg, end = i+1, len(nums)-1
+            while beg < end:
+                sum = nums[i] + nums[beg] + nums[end]
+                if sum < 0:
+                    beg += 1
+                elif sum > 0:
+                    end -= 1
+                else:
+                    result.append((nums[i],nums[beg],nums[end]))
+                    while beg < end and nums[beg] == nums[beg+1]:
+                        beg += 1
+                    while beg < end and nums[end] == nums[end-1]:
+                        end -= 1
+                    beg += 1
+                    end -= 1
         return result
+
+
 
 nums = [-1,0,1,2,-1,-4]
 
