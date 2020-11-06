efb361b2cb219b6f6d86a2cea53dc7ab3e591687
Varshini <varshini.reddy4@gmail.com>
2020-11-06 14:37:44 -0600

    height of binary search tree

---
+++ Trees&Graphs/Minimum depth/ height.py
@@ -0,0 +1,19 @@
+class TreeNode:
+    def __init__(self,val=0,left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def minDepth(self,root):
+        if root is None:
+            return 0
+        if root.left is None and root.right is None:
+            return 1
+        if root.left is None:
+            return self.minDepth(root.right) + 1
+        if root.right is None:
+            return self.minDepth(root.left) + 1
+        return min(self.minDepth(root.left),self.minDepth(root.right))+1
+
+root = TreeNode(1)
